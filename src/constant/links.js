9ad346edafb440d2d34250f654d8af74f68bed1a
Andrew Mos <andresmosquera739@gmail.com>
2026-06-21 23:09:09 -0500

    first commit

---
+++ src/constant/links.js
@@ -0,0 +1,2 @@
+export const EMAIL = 'a.mosquerah2@uniandes.edu.co'
+export const GITHUB_URL = 'https://github.com/andresmosqueraw/bias-thought-anchors.git'
---
+++ src/constant/navbar.js
@@ -0,0 +1,22 @@
+export const navbarItems = [
+    {
+        name: '🎥 Video',
+        href: '/',
+    },
+    {
+        name: '💻 Code',
+        href: 'https://github.com/andresmosqueraw/bias-thought-anchors.git',
+    },
+    {
+        name: '📊 Dataset',
+        href: 'https://github.com/andresmosqueraw/bias-thought-anchors.git',
+    },
+    {
+        name: '📄 Paper',
+        href: 'https://github.com/andresmosqueraw/bias-thought-anchors.git',
+    },
+    {
+        name: 'Contact',
+        href: 'mailto:a.mosquerah2@uniandes.edu.co',
+    }
+]
